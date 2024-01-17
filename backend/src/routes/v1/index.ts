87246df96254d05b301642169e1475d36916f8ce
karimou <karimou.ba@gmail.com>
2024-01-17 19:26:42 +0000

    [KBa] Add persons routes

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from './users';
 import electionsRouter from './elections';
 import organisationsRouter from './organisations';
+import personsRouter from './persons';
 
 const v1Router = Router();
 
 v1Router.use('/users', usersRouter);
 v1Router.use('/elections', electionsRouter);
 v1Router.use('/organisations', organisationsRouter);
+v1Router.use('/persons', personsRouter);
 
 export default v1Router;
---
+++ backend/src/routes/v1/persons/index.ts
@@ -0,0 +1,85 @@
+
+import { Router, Request, Response } from 'express';
+
+import { Person, User } from '../../../models';
+
+import { isUserSigned } from '../../../middleware';
+
+const router = Router();
+
+router
+    .get('/:personId', isUserSigned, async (req: Request, res: Response) => {
+        try {
+            
+            let person = await Person
+                .getById(req.params.personId);
+            res.status(200).send(person);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+            
+        }
+
+    })
+
+router.route('')
+    .all(isUserSigned)
+    .get(async (_req: Request, res: Response) => {
+
+        try {
+            
+            let persons = await Person
+                .list();
+            res.status(200).send(persons);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+        
+    })
+    .post(async (req: Request, res: Response) => {
+
+        try {
+            
+            let person = new Person(req.body, req.session?.user as User);
+            await person.add();
+            res.status(200).send(person);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+    })
+    .put(async (req: Request, res: Response) => {
+
+        try {
+
+            let person = new Person(req.body, req.session?.user as User);
+            await person.update();
+            res.status(200).send(person);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+
+    })
+    .delete(async (req: Request, res: Response) => { 
+
+        try {
+
+            await Person
+                .delete(req.body.ids);
+
+        } catch (e) {
+            return res.status(500).send(e);
+        }
+    })
+
+
+export default router;
