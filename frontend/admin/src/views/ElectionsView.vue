e8c280c43044ce30ce12879dc76024d784c36856
karimou <karimou.ba@gmail.com>
2024-01-17 11:01:12 +0000

    [KBa] Add Sakai template support

--- frontend/admin/src/views/ElectionsView.vue
+++ frontend/admin/src/views/ElectionsView.vue
@@ -1,5 +1,14 @@
+<script setup>
+    import DataTable from 'primevue/datatable';
+    
+</script>
 <template>
-    <div>
-        ElectionsView
+    <div class="grid">
+        <div class="col-12">
+            <div class="card">
+                <h5>Elections view</h5>
+                <DataTable />
+            </div>
+        </div>
     </div>
 </template>
