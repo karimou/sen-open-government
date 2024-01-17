ba022b14df72b6258aa06a4c573991ef4bad72cc
karimou <karimou.ba@gmail.com>
2024-01-17 22:11:23 +0000

    [KBa] add issues and opinions endpoints

---
+++ backend/src/routes/v1/issues/index.ts
@@ -0,0 +1,85 @@
+
+import { Router, Request, Response } from 'express';
+
+import { Issue, User } from '../../../models';
+
+import { isUserSigned } from '../../../middleware';
+
+const router = Router();
+
+router
+    .get('/:issueId', isUserSigned, async (req: Request, res: Response) => {
+        try {
+            
+            let issue = await Issue
+                .getById(req.params.issueId);
+            res.status(200).send(issue);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+            
+        }
+
+    })
+
+router.route('')
+    .all(isUserSigned)
+    .get(async (_req: Request, res: Response) => {
+
+        try {
+            
+            let issues = await Issue
+                .list();
+            res.status(200).send(issues);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+        
+    })
+    .post(async (req: Request, res: Response) => {
+
+        try {
+            
+            let issue = new Issue(req.body, req.session?.user as User);
+            await issue.add();
+            res.status(200).send(issue);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+    })
+    .put(async (req: Request, res: Response) => {
+
+        try {
+
+            let issue = new Issue(req.body, req.session?.user as User);
+            await issue.update();
+            res.status(200).send(issue);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+
+    })
+    .delete(async (req: Request, res: Response) => { 
+
+        try {
+
+            await Issue
+                .delete(req.body.ids);
+
+        } catch (e) {
+            return res.status(500).send(e);
+        }
+    })
+
+
+export default router;
---
+++ backend/src/routes/v1/opinions/index.ts
@@ -0,0 +1,85 @@
+
+import { Router, Request, Response } from 'express';
+
+import { Opinion, User } from '../../../models';
+
+import { isUserSigned } from '../../../middleware';
+
+const router = Router();
+
+router
+    .get('/:opinionId', isUserSigned, async (req: Request, res: Response) => {
+        try {
+            
+            let opinion = await Opinion
+                .getById(req.params.opinionId);
+            res.status(200).send(opinion);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+            
+        }
+
+    })
+
+router.route('')
+    .all(isUserSigned)
+    .get(async (_req: Request, res: Response) => {
+
+        try {
+            
+            let opinions = await Opinion
+                .list();
+            res.status(200).send(opinions);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+        
+    })
+    .post(async (req: Request, res: Response) => {
+
+        try {
+            
+            let opinion = new Opinion(req.body, req.session?.user as User);
+            await opinion.add();
+            res.status(200).send(opinion);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+    })
+    .put(async (req: Request, res: Response) => {
+
+        try {
+
+            let opinion = new Opinion(req.body, req.session?.user as User);
+            await opinion.update();
+            res.status(200).send(opinion);
+
+        } catch (e) {
+
+            return res.status(500).send(e);
+
+        }
+
+    })
+    .delete(async (req: Request, res: Response) => { 
+
+        try {
+
+            await Opinion
+                .delete(req.body.ids);
+
+        } catch (e) {
+            return res.status(500).send(e);
+        }
+    })
+
+
+export default router;
