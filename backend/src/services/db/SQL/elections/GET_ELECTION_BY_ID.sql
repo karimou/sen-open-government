36e4d969ec574f479ccb679292b158c9cb1fc0e6
karimou <karimou.ba@gmail.com>
2024-01-17 14:52:45 +0000

    [KBa] Election data model and endpoints

--- backend/src/services/db/SQL/elections/GET_ELECTION_BY_ID.sql
+++ backend/src/services/db/SQL/elections/GET_ELECTION_BY_ID.sql
@@ -4,8 +4,8 @@ SELECT
     election.type, 
     election.description, 
     election.article_url as article, 
-    election.created_on as createdOn, 
-    election.last_modified_on as lastModifiedOn, 
+    election.created_on, 
+    election.last_modified_on, 
     json_build_object('id', "user".id, 'username', "user".username) as user
 FROM election
 LEFT JOIN "user" ON election.last_modified_by = "user".id
