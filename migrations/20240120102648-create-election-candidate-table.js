f316a9b588d3e6f302daa994fcff4fd6bfe71b53
karimou <karimou.ba@gmail.com>
2024-01-20 10:38:55 +0000

    [KBa] Election candidate data model

--- migrations/20240120102648-create-election-candidate-table.js
+++ migrations/20240120102648-create-election-candidate-table.js
@@ -15,7 +15,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db, callback) {
-  db.createdTable('election_candidate', {
+  db.createTable('election_candidate', {
     id: {
       type: 'int',
       primaryKey: true,
@@ -66,13 +66,29 @@ exports.up = function(db, callback) {
       }
 		}
   }, function(err) {
-    if (err) {console.log(err);return callback(err)}
-    callback();
-  });
+    if (err) {console.log(err);return callback(err)};
+    let query = `
+      CREATE UNIQUE INDEX IF NOT EXISTS election_candidate_combined_index ON election_candidate(election_id, person_id);
+      ALTER TABLE election_candidate ADD CONSTRAINT unique_label UNIQUE USING INDEX election_candidate_combined_index;
+    `;
+    db.runSql(query, function(err) {
+      if (err) {console.log(err);return callback(err)}
+      return callback();
+    });
+  })
 };
 
+
 exports.down = function(db, callback) {
-  db.dropTable('election_candidate');
+  let query = `
+    DROP INDEX IF NOT EXISTS election_candidate_combined_index;
+    ALTER TABLE election_candidate DROP CONSTRAINT IF EXISTS unique_label;
+  `;
+  
+  db.runSql(query, function(err) {
+    if (err) {console.log(err);return callback(err)};
+    db.dropTable('election_candidate', callback);
+  });
 };
 
 exports._meta = {
