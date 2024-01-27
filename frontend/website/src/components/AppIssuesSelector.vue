0cc33b236db458eabe2fcf6235f3586cdb3af327
karimou <karimou.ba@gmail.com>
2024-01-27 16:04:56 +0000

    [KBa] minor UX update

--- frontend/website/src/components/AppIssuesSelector.vue
+++ frontend/website/src/components/AppIssuesSelector.vue
@@ -1,5 +1,6 @@
 <script setup>
     import Button from 'primevue/button';
+    import Avatar from 'primevue/avatar';
     import { useElectionsStore } from '@/stores/elections';
 
     const electionsStore = useElectionsStore();
@@ -17,12 +18,18 @@
 
     <Button 
         v-for="issue in electionsStore.currentElection.issues"
-        :label="issue.title" 
         class="mx-1" 
         text
         size="small"
         severity="primary"
         @click="scrollToIssue(issue.id, $event)"
-    />
+    >
+        <Avatar 
+            :image="issue.photo"
+            shape="circle"
+            class="mr-1"
+        />
+        <span class="p-button-label">{{ issue.title }}</span>
+    </Button>
 
 </template>
