8d897f9f514f6ef0154e9abfa68d6026b0ff2040
karimou <karimou.ba@gmail.com>
2024-01-27 19:04:32 +0000

    [KBa] Add DS website

--- frontend/website/src/views/ElectionView.vue
+++ frontend/website/src/views/ElectionView.vue
@@ -23,7 +23,7 @@
     <nav class="flex align-items-center justify-content-center">
         <div class="logo" style="position: absolute; left: 10px">
             <!-- Logo Placeholder for Illustration -->
-            <a href="#home"><img width="80px" height="auto" :src="logoDs"></a>
+            <a href="https://www.demainsenegal.sn/" target="_blank"><img width="80px" height="auto" :src="logoDs"></a>
         </div>
         <RouterLink custom v-slot="{ href, navigate }" :to="`/election/${ electionsStore.currentElection.id }`">
             <a v-ripple :href="href" @click="navigate" class="title">
