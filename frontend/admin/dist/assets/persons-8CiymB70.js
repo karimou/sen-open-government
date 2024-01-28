faffbac64a2c06c9da1677372776f8fc481e8d96
karimou <karimou.ba@gmail.com>
2024-01-28 19:28:13 +0000

    [KBa] build

---
+++ frontend/admin/dist/assets/persons-8CiymB70.js
@@ -0,0 +1,1 @@
+import{d as r,r as n,A as o}from"./index-t5PwyJpr.js";const u=r("persons",()=>{const s=n(null);return{persons:s,refreshPersons:async()=>o.persons.listPersons().then(e=>s.value=e)}});export{u};
