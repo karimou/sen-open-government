cf15e95eb3375d74ebcf30d6fd63f1d5bd703c12
karimou <karimou.ba@gmail.com>
2024-01-28 18:38:31 +0000

    [KBa] minor fix + build

--- frontend/website/src/views/ElectionMain.vue
+++ frontend/website/src/views/ElectionMain.vue
@@ -89,7 +89,7 @@
 	</div>
 	<Divider />
 	<div class="grid">
-		<div class="lg:col-offset-3 lg:col-6 md:col-offset-2 md:col-8">
+		<div class="lg:col-offset-3 lg:col-6 md:col-offset-2 md:col-8 col">
 			<ElectionMainOpinionsByAuthor 
 				v-if="isGroupBy('author_id')"
 				:groupedOpinions="groupedOpinions"
