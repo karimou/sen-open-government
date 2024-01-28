645186098cc53dfb6d85b9c685f3e50c8857cdf6
karimou <karimou.ba@gmail.com>
2024-01-28 18:46:35 +0000

    [KBa] candidates social links

--- frontend/website/src/components/AppSocialLinks.vue
+++ frontend/website/src/components/AppSocialLinks.vue
@@ -20,13 +20,15 @@
 
     const { website, facebook, twitter, instagram } = toRefs(props);
 
+    const goToUrl = (url) => window.open(url, '_blank');
+
 </script>
 <template>
     <div class="flex justify-content-center">
-        <Button icon="pi pi-link" text v-if="website"/>
-        <Button icon="pi pi-facebook" text v-if="facebook"/>
-        <Button icon="pi pi-twitter" text v-if="twitter"/>
-        <Button icon="pi pi-instagram" text v-if="instagram"/>
+        <Button icon="pi pi-link" text v-if="website" @click="goToUrl(website)"/>
+        <Button icon="pi pi-facebook" text v-if="facebook" @click="goToUrl(facebook)"/>
+        <Button icon="pi pi-twitter" text v-if="twitter" @click="goToUrl(twitter)"/>
+        <Button icon="pi pi-instagram" text v-if="instagram" @click="goToUrl(instagram)"/>
     </div>
 
 
