65cfbf98c4ee9c7c25474118815b71b47f6c9190
karimou <karimou.ba@gmail.com>
2024-01-29 11:35:31 +0000

    [KBa] minor fix

--- frontend/website/src/App.vue
+++ frontend/website/src/App.vue
@@ -20,7 +20,7 @@
   nav {
     height: 76px;
     /* padding: 5px 10px 5px 20px; */
-    /* background-color: #fcb900; */
+    background-color: white;
     border-bottom: 3px solid #fcb900;
     position: fixed;
     top: 0;
