5b7656845d539a288dc21289ad44adad18ad392b
karimou <karimou.ba@gmail.com>
2024-01-31 14:25:02 +0000

    [KBa] build

--- frontend/admin/dist/assets/persons-Z1npBqe_.js
+++
@@ -1,1 +0,0 @@
-import{d as r,r as n,A as o}from"./index-J8nPbcPQ.js";const u=r("persons",()=>{const s=n(null);return{persons:s,refreshPersons:async()=>o.persons.listPersons().then(e=>s.value=e)}});export{u};
