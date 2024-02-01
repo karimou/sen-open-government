5bc70ebb196c7e5e4219c9c29d14c375d03b4e60
karimou <karimou.ba@gmail.com>
2024-02-01 17:16:56 +0000

    [KBa] build

--- frontend/website/dist/assets/ElectionsView-H0WqXZrM.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{c,o}from"./index-NB82Ecj1.js";const t={};function n(r,s){return o(),c("div",null," Elections view ")}const a=e(t,[["render",n]]);export{a as default};
